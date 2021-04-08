48e83f890bcc730a287548fa2a25a1aaa995bc0c
YUSUKE411997 <hy.skyblue.41@icloud.com>
2021-04-08 12:53:25 +0000

    [finish]4-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,12 +6,15 @@ class BooksController < ApplicationController
     @book = Book.new
   end
   
+    def show
+    @book = Book.find(params[:id])
+  end
   
   def create
     @book = Book.new(book_params)
   if @book.save
      flash[:notice] = "successfully"
-    redirect_to books_path
+    redirect_to books_show_path(@book)
   else
     @books = Book.all.order(created_at: :desc)
     render :index
@@ -21,9 +24,7 @@ class BooksController < ApplicationController
   end 
 
 
-  def show
-    @book = Book.find(params[:id])
-  end
+
 
   def edit
     @book = Book.find(params[:id])
